7da1d78711f7e1155b2d7b79de85180530d654ad
dnjsqhwo <dnjsqhwo@gmail.com>
2022-11-27 01:21:48 +0900

    chore: 플러그인 done 추가

--- src/plugins/route-plugin.ts
+++ src/plugins/route-plugin.ts
@@ -1,11 +1,11 @@
 import { FastifyPluginCallback } from 'fastify';
 import fp from 'fastify-plugin';
 import userRoutes from '../modules/user/user.route';
-import productRoutes from '../modules/product/product.route';
 
-const routePlugin: FastifyPluginCallback = (fastify) => {
+const routePlugin: FastifyPluginCallback = (fastify, options, done) => {
   fastify.register(userRoutes, { prefix: 'api/users' });
-  fastify.register(productRoutes, { prefix: 'api/products' });
+
+  done();
 };
 
 export default fp(routePlugin);
