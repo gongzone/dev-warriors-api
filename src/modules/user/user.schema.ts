42eed7d912a46eb0704336450060bb6ee3e0c3bd
dnjsqhwo <dnjsqhwo@gmail.com>
2022-11-27 01:22:32 +0900

    chore: user 스키마 재작성

--- src/modules/user/user.schema.ts
+++ src/modules/user/user.schema.ts
@@ -1,52 +1,18 @@
 import { z } from 'zod';
 import { buildJsonSchemas } from 'fastify-zod';
 
-const userCore = {
-  email: z
-    .string({
-      required_error: 'Email is required',
-      invalid_type_error: 'Email must be a string',
-    })
-    .email(),
-  name: z.string(),
-};
+import { signupSchema, signupResponseSchema } from './schema/singup';
+import { loginSchema, loginResponseSchema } from './schema/login';
 
-const createUserSchema = z.object({
-  ...userCore,
-  password: z.string({
-    required_error: 'Password is required',
-    invalid_type_error: 'Password must be a string',
-  }),
-});
-
-const createUserResponseSchema = z.object({
-  id: z.number(),
-  ...userCore,
-});
-
-const loginSchema = z.object({
-  email: z
-    .string({
-      required_error: 'Email is required',
-      invalid_type_error: 'Email must be a string',
-    })
-    .email(),
-  password: z.string(),
-});
-
-const loginResponseSchema = z.object({
-  accessToken: z.string(),
-});
-
-export type CreateUserDto = z.infer<typeof createUserSchema>;
+export type SignupDTO = z.infer<typeof signupSchema>;
 export type LoginUserDto = z.infer<typeof loginSchema>;
 
 export const { schemas: userSchemas, $ref } = buildJsonSchemas(
   {
-    createUserSchema,
-    createUserResponseSchema,
+    signupSchema,
+    signupResponseSchema,
     loginSchema,
-    loginResponseSchema,
+    loginResponseSchema
   },
   { $id: 'userSchemas' }
 );
